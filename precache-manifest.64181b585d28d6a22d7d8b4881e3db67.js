dfaec70a4fc27da616776a5756836c78fa4c2fde
Amel <zekafrombtm@hotmail.com>
2020-03-24 19:40:26 +0100

    Updates

---
+++ precache-manifest.64181b585d28d6a22d7d8b4881e3db67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7ab8380cac52dab4f0ca319e242aca",
+    "url": "/monsters-search/index.html"
+  },
+  {
+    "revision": "3254c9098bae14751f0a",
+    "url": "/monsters-search/static/css/main.ffaba8f5.chunk.css"
+  },
+  {
+    "revision": "c1ab0b70d58f976214a2",
+    "url": "/monsters-search/static/js/2.10564fa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-search/static/js/2.10564fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3254c9098bae14751f0a",
+    "url": "/monsters-search/static/js/main.63ab5cf5.chunk.js"
+  },
+  {
+    "revision": "a9ef89b4715d8d46c44c",
+    "url": "/monsters-search/static/js/runtime-main.126e97eb.js"
+  }
+]);
