64c8912b41fe404daa94824fbc913182191f8433
Amel <zekafrombtm@hotmail.com>
2020-04-19 14:14:52 +0200

    2nd commit

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,13 +3,15 @@ import './card.css';
 
 
 
-const Card = props =>{
+const Card = props => {
    
     return (
     <div className="card-container">
     <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster"></img>
-   <h2>{props.monster.name}</h2>
-   <p>{props.monster.email}</p> 
+        <div className="nameandemail">
+        <h2>{props.monster.name}</h2>
+        <p>{props.monster.email}</p>
+        </div>
    </div>
     );
 
